42158735e31fc3a77ed77996fa1b9f2f6dedfc5c
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-02 14:16:02 -0400

    step 1

--- people.rb
+++ people.rb
@@ -1,9 +1,25 @@
-class Student
- def learn
-   p "I get it!"
- end
+class Parent
+  attr_accessor :name
+
+def walk
+  "#{@name} is walking to school"
 end
 
+parent1 = Parent.new
+parent1 = "John"
+p parent1.name
+p parent1.walk
 
-class Instructor
-end
+
+# class Student
+#  def learn
+#    puts "I get it!"
+#  end
+# end
+#
+#
+# class Instructor
+#   def teach
+#     puts "Everything in Ruby is an Object"
+#   end
+# end
