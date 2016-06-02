a80488ee3257afb679fbf0ab5eaf448558362f36
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-02 14:42:51 -0400

    added instance

--- cat.rb
+++ cat.rb
@@ -1,2 +1,15 @@
 class Cat
+# attr_accessor :name, :preferred_food, :meal_time
+
+def initialize(name, preferred_food, meal_time)
+  @name = name
+  @preferred_food = preferred_food
+  @meal_time = meal_time
 end
+
+end
+#initializing with initialize
+marble = Cat.new("marble", "tuna", "5pm")
+
+
+#initialize with attr_accessor
