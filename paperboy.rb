874523ce56988d6b496f052a12014e9bc6bf7afe
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-03 15:16:52 -0400

    woohoo, finished paperboy

--- paperboy.rb
+++ paperboy.rb
@@ -2,24 +2,41 @@ class Paperboy
   attr_accessor :name, :quota, :experience, :side
   attr_reader :earnings
 
-  def initialize(name, quota, experience, side, earnings)
+  def initialize(name, side)
     @name = name
-    @quota = quota
-    @experience = experience
+    @quota = 50
+    @experience = 0
     @side = side
-    @earnings = earnings
+    @earnings = 0
   end
 
   def quota
-    (@experience / 2) + 50
+    @quota = (@experience / 2) + 50
   end
 
   def deliver(start_address, end_address)
-
+    #number of deliveries =
+    num_deliveries = (end_address - start_address) / 2
+    #@experience += num_deliveries <--This is wrong
+    num_deliveries.times do |x|
+      @experience += 1
+      if x > @quota
+        @earnings += 0.50
+      elsif x <= @quota
+        @earnings += 0.25
+      end
+    end
+    @earnings -= 2 if num_deliveries < @quota
   end
 
+
+
   def report
-    "I'm #{name}, and I've delivered #{experience}, and I've earned #{earnings} so far."
+    "I'm #{@name}, and I've delivered #{@experience}, and I've earned #{@earnings} so far."
   end
 
 end
+puts ellie = Paperboy.new("Ellie", "even")
+ ellie.deliver(400, 800)
+puts ellie.experience
+puts ellie.report
