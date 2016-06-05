5a1dd255bc061a0ae1fd3a9de4825c806b7201be
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-05 16:19:02 -0400

    updated methods

--- mars.rb
+++ mars.rb
@@ -1,3 +1,9 @@
+#put all your gets chomp shit up here
+#1. define plateau size
+#2. give position (x, y, direction) for rover 1
+#3. give instructions (L, R, M) for rover 1
+#4,5. repeat #2&3 for rover 2
+
 class Rover
   attr_accessor :x, :y, :direction
 
@@ -7,18 +13,22 @@ class Rover
     @direction = direction
   end
 
-  def read_instruction
+  def read_instruction(instruction)
+    instruction.each do |command|
+      if command == "R" || command == "L"
+        turn(command)
+      elsif command == "M"
+        move(command)
+      end
+    end
+    
+    #accepts instructions. decides move or turn
   end
 
-  def move
+  def move #updates x, y coordinates
   end
 
-  def turn
+  def turn #turns R or L
   end
 
-end
-
-
-
-class Nasa
 end
