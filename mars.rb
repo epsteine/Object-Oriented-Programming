a3d8118b9ecd1a1a929181449867a6dce7f008fe
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-05 15:49:59 -0400

    setting up mars rovers

---
+++ mars.rb
@@ -0,0 +1,24 @@
+class Rover
+  attr_accessor :x, :y, :direction
+
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
+
+  def read_instruction
+  end
+
+  def move
+  end
+
+  def turn
+  end
+
+end
+
+
+
+class Nasa
+end
